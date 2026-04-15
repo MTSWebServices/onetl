e5b6012346d3ff086a962ebc1aeefd11753ee136
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2026-04-15 08:39:42 +0000

    [DOP-36284] Fix DBWriter tests

--- tests/tests_unit/test_db/test_db_writer_unit/test_clickhouse_writer_unit.py
+++ tests/tests_unit/test_db/test_db_writer_unit/test_clickhouse_writer_unit.py
@@ -7,10 +7,18 @@ pytestmark = pytest.mark.clickhouse
 
 
 def test_clickhouse_writer_wrong_table_name(spark_mock):
-    clickhouse = Clickhouse(host="some_host", user="user", database="database", password="passwd", spark=spark_mock)
+    clickhouse_with_db = Clickhouse(
+        host="some_host", user="user", database="database", password="passwd", spark=spark_mock
+    )
+    clickhouse_without_db = Clickhouse(host="some_host", user="user", password="passwd", spark=spark_mock)
+
+    DBWriter(
+        connection=clickhouse_with_db,
+        target="table",
+    )
 
     with pytest.raises(ValueError, match=r"Name should be passed in `schema\.name` format"):
         DBWriter(
-            connection=clickhouse,
+            connection=clickhouse_without_db,
             target="table",  # Required format: target="schema.table"
         )
--- tests/tests_unit/test_db/test_db_writer_unit/test_mysql_writer_unit.py
+++ tests/tests_unit/test_db/test_db_writer_unit/test_mysql_writer_unit.py
@@ -7,10 +7,16 @@ pytestmark = pytest.mark.mysql
 
 
 def test_mysql_writer_wrong_table_name(spark_mock):
-    mysql = MySQL(host="some_host", user="user", database="database", password="passwd", spark=spark_mock)
+    mysql_with_db = MySQL(host="some_host", user="user", database="database", password="passwd", spark=spark_mock)
+    mysql_without_db = MySQL(host="some_host", user="user", password="passwd", spark=spark_mock)
+
+    DBWriter(
+        connection=mysql_with_db,
+        target="table",
+    )
 
     with pytest.raises(ValueError, match=r"Name should be passed in `schema\.name` format"):
         DBWriter(
-            connection=mysql,
+            connection=mysql_without_db,
             target="table",  # Required format: target="schema.table"
         )
